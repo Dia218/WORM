455ef5b09c70f030bea93f58e86e9fc674b75be3
dia218 <sang218@kyonggi.ac.kr>
2022-05-05 12:35:58 +0900

    0505

---
+++ src/System/GamePlay.java
@@ -0,0 +1,14 @@
+package System;
+
+//게임을 동작시키는 메인 클래스
+public class GamePlay {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/System/KeyInput.java
@@ -0,0 +1,6 @@
+package System;
+
+//사용자의 키 입력을 받아 동작 시키는 클래스
+public class KeyInput {
+
+}
