4fb90178bd768e0d9990f3807970bdc033053179
Dia218 <sang218@kyonggi.ac.kr>
2022-05-12 16:34:57 +0900

    0512 병합 전 커밋

--- WormGame/src/create/BlockCreate.java
+++ WormGame/src/create/BlockCreate.java
@@ -6,19 +6,19 @@ public class BlockCreate {
 	
 	
 	//기본 블록 생성 - 3개?
+	void createBasicBlock() {
+		
+		BasicBlock basicblock = new BasicBlock(blockX, blockY, blockLength, blockWidth);
+		
+	}
+	
+	
 	
 	int blockX = (int)Math.random(); //겹치지 않게...?
 	int blockY = (int)Math.random(); //겹치지 않게...?
 	int blockLength = (int)Math.random(); //3이하로 수정
 	int blockWidth = (int)Math.random(); //3이하로 수정
 		
-	BasicBlock basicblock1 = new BasicBlock(blockX, blockY, blockLength, blockWidth);
-	
-	
-	
-	
-	
-	//출구 블록 생성 - 1개 고정
 	
 	
 	
--- WormGame/src/system/ScreenBox.java
+++ WormGame/src/system/ScreenBox.java
@@ -8,23 +8,25 @@ public class ScreenBox extends JPanel {
 	int blocksize = 10;
 	
 	//게임 보드 2차원 배열
-	int[][] gameboard;
-	gameboard = new int[20][20];
+	int [][] gameboard = new int[20][20];
+
+	//게임 보드 초기화를 진행시키는 메소드
+	void setboard() {
+		//게임보드 초기화 = 1
+	    for(int y = 0; y < 20; y++) {
+	    	for(int x = 0; x < 20; x++) {
+	    		this.gameboard[x][y] = 1;
+	        }
+	    }
+	}
 	
-	//게임보드 초기화 = 1
-    for(int y = 1; y <= 20; y++) {
-    	for(int x = 1; x <= 20; x++) {
-    		gameboard[x][y] = 1;
-        }
-    }
+
 	
 	//게임 보드에 네모칸 그리기
 	@Override
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
-		
-
 		
         for(int y = 0; y < 20; y++) {
             for(int x = 0; x < 20; x++){
