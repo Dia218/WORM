74c10b468fd3624d228be6055d2885484c339b50
honey720 <honey720@naver.com>
2022-05-12 22:18:37 +0900

    0512 이동헌 지렁이 키입력 수정1

--- WormGame/src/system/KeyInput.java
+++ WormGame/src/system/KeyInput.java
@@ -6,47 +6,45 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.util.TimerTask;
 
-//사용자의 키 입력을 받아 동작 시키는 클래스
-public class KeyInput extends ControlGame {
-	private Head head = new Head(); 	//지렁이 머리 패널!
-	
+public class KeyInput extends GamePlay { //메인 메소드에서 KeyInput 생성 필요!
+	private Head head = new Head();		//지렁이 머리 생성. worm쪽에서 지렁이 머리 패널 가져와서 쓸것!
 	public KeyInput() {
 	
-	Container c = getContentPane();		//컨테이너 c 생성
-	c.setLayout(null);					
-	
+	Container c = getContentPane();		//컨테이너 생성.
+	c.setLayout(null);					//이거 필요한가?
+
 	c.addKeyListener(new MyKeyListener());
-	head.setLocation(WormCreate.X, WormCreate.Y); //지렁이 머리 위치 설정. create 에서 생성한 좌표 값 입력!
-	head.setSize(Worm.X, Worm.Y); 			//지렁이 머리 크기 설정. 지렁이 객체에서 생성한 크기 입력!
-	c.add(head);
-	c.setFocusable(true); // 컨텐트팬이 포커스를 받을 수 있도록 설정
-	c.requestFocus(); // 포커스 지정
+	head.setLocation(WormCreate.X, WormCreate.Y);	//지렁이 머리 패널 생성 위치. WormCreate에서 생성한 지렁이 머리 좌표 여기서 쓰던지 저쪽에서 쓰던지!
+	head.setSize(Worm.X, Worm.Y);		//지렁이 머리 크기. Worm쪽에서 지렁이 머리 가져와서 쓰던지 저쪽에서 쓰던지
+	c.add(head);						//머리 컨테이너 추가
+	c.setFocusable(true);				// 컨텐트팬이 포커스를 받을 수 있도록 설정
+	c.requestFocus();					// 포커스 지정
 	
 	}	
 
 	class MyKeyListener extends KeyAdapter {
 
-		int keyCodetmp = 0;	// 방금 전에 입력한 키코드 변수
-		java.util.Timer timer = new java.util.Timer();	//타이머 객체 생성
-		TimerTask task;		//TimerTask 선언
-		public void keyPressed(KeyEvent e) {	//키가 입력될 때
+		int keyCodetmp = 0;				//방금전 입력한 키코드
+		java.util.Timer timer = new java.util.Timer();
+		TimerTask task;
+		public void keyPressed(KeyEvent e) {
 			
 			int keyCode = e.getKeyCode(); // 입력된 키코드
-			switch(keyCode) {		//입력된 키코드
+			switch(keyCode) {
 		
-			case KeyEvent.VK_UP: {	//위쪽 방향키를 누를 떄
-				if(keyCodetmp == KeyEvent.VK_DOWN || keyCodetmp == KeyEvent.VK_UP) 	//같은 방향키나 반대 방향키를 누를 때
-					break;			//이 switch 문은 생략한다.
-				else if(task != null)	//TimerTask가 실행될 때
-					task.cancel();		//이전 TimerTask를 종료한다.
+			case KeyEvent.VK_UP: {			//위쪽 방향키를 입력할 경우
+				if(keyCodetmp == KeyEvent.VK_DOWN || keyCodetmp == KeyEvent.VK_UP) //같은 방향키나 반대 방향키를 입력했다면
+					break;					//탈출한다.
+				else if(task != null)		//TimerTask가 생성되었다면
+					task.cancel();			//TimreTask를 종료한다.
 				
-				keyCodetmp = KeyEvent.VK_UP;	//방금 전에 입력한 키코드 변수에 위쪽 방향키를 넣는다.
-				task = new UP();	//TimerTask UP 객체 생성
-				timer.schedule(task, 0, 100);	//0.1초마다 task를 반복하는 스케쥴을 즉시 시행한다.
+				keyCodetmp = KeyEvent.VK_UP;//방금전 입력한 키코드 변수에 현재 입력한 방향키를 넣는다.
+				task = new UP();			//TimerTask 생성
+				timer.schedule(task, 0, 100);//task를 0.1초간 반복하는 schedule을 즉시(0초) 생성한다.
 				break;
 			}
 			
-			case KeyEvent.VK_DOWN: {	//아랫쪽 방향키를 누를 때
+			case KeyEvent.VK_DOWN: {		//아랫쪽 방향키를 입력할 경우
 				if(keyCodetmp == KeyEvent.VK_UP || keyCodetmp == KeyEvent.VK_DOWN) 
 					break;
 				else if(task != null)
@@ -58,19 +56,19 @@ public class KeyInput extends ControlGame {
 				break;
 			}
 			
-			case KeyEvent.VK_LEFT: {	//왼쪽 방향키를 누를 때
+			case KeyEvent.VK_LEFT: {		//왼쪽 방향키를 입력할 경우
 				if(keyCodetmp == KeyEvent.VK_RIGHT || keyCodetmp == KeyEvent.VK_LEFT) 
 					break;
 				else if(task != null)
 					task.cancel();
-			
+				
 				keyCodetmp = KeyEvent.VK_LEFT;
 				task = new LEFT();
 				timer.schedule(task, 0, 100);
 					break;
 			}	
 			
-			case KeyEvent.VK_RIGHT: {	//오른쪽 방향키를 누를 때
+			case KeyEvent.VK_RIGHT: {		//오른쪽 방향키를 입력할 경우
 				if(keyCodetmp == KeyEvent.VK_LEFT || keyCodetmp == KeyEvent.VK_RIGHT) 
 					break;
 				else if(task != null)
@@ -86,26 +84,25 @@ public class KeyInput extends ControlGame {
 			
 			}
 		}
-
 	
-	class UP extends TimerTask {
+	class UP extends TimerTask {	//위로 이동
 		public void run() {
-			head.setLocation(head.getX(), head.getY() - value); //	지렁이 머리를 x, y 좌표로 옮긴다. value값은 맵에서 생성한 격자 크기만큼 옮길 것!
+			head.setLocation(head.getX(), head.getY() - value); 	//현재 지렁이 머리 좌표에서 value값 만큼 이동.(value 값은 맵 격자만큼 이동)
 		}
 	}
-	class DOWN extends TimerTask {
-		public void run() {
-
-			head.setLocation(head.getX(), head.getY() + value);
-		}
-	}
-	class LEFT extends TimerTask {
+	class DOWN extends TimerTask {	//아래로 이동
 		public void run() {
 
-			head.setLocation(head.getX() - vlaue, head.getY());
+			head.setLocation(head.getX(), head.getY() + vlaue);
 		}
 	}
-	class RIGHT extends TimerTask {
+	class LEFT extends TimerTask {	//왼쪽으로 이동
+		public void run() {
+
+			head.setLocation(head.getX() - value, head.getY());
+		}
+	}
+	class RIGHT extends TimerTask {	//오른쪽으로 이동
 		public void run() {
 
 			head.setLocation(head.getX() + value, head.getY());
@@ -113,3 +110,4 @@ public class KeyInput extends ControlGame {
 	}
 	
 }
+
