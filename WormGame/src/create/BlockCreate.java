17126e711991032ce8a135397a7771b999cc9f22
Dia218 <sang218@kyonggi.ac.kr>
2022-05-12 21:33:55 +0900

    0512 작업이디ㅗㅇ

--- WormGame/src/create/BlockCreate.java
+++ WormGame/src/create/BlockCreate.java
@@ -4,20 +4,41 @@ import block.BasicBlock;
 //블록을 생성시키는 클래스
 public class BlockCreate {
 	
+	//랜덤 좌표 생성기
+	void locateBlock() {
+		int blockX = (int)(Math.random() * 19) + 0;
+		int blockY = (int)(Math.random() * 19) + 0;
+		
+		if() {
+			locateBlock();
+		}
+		
+		
+		
+	}
+	
 	
 	//기본 블록 생성 - 3개?
 	void createBasicBlock() {
 		
-		BasicBlock basicblock = new BasicBlock(blockX, blockY, blockLength, blockWidth);
+		BasicBlock[] basicblock;
+		basicblock = new BasicBlock[5];
+		
+		for(int i = 1; i <= 5; i++) {
+			basicblock[i] = 
+
+			
+		}
+		
+		
+		
+		
 		
 	}
 	
 	
 	
-	int blockX = (int)Math.random(); //겹치지 않게...?
-	int blockY = (int)Math.random(); //겹치지 않게...?
-	int blockLength = (int)Math.random(); //3이하로 수정
-	int blockWidth = (int)Math.random(); //3이하로 수정
+
 		
 	
 	
